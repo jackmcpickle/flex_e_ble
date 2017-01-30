5a4b8e0a15830d88a2b4c82ed6cfb88796f3aae6
Jack McNicol <jackmcpickle@gmail.com>
2017-01-31 01:55:19 +1030

    update packages - remove coffeescript

---
+++ gulpfile.js
@@ -0,0 +1,103 @@
+const gulp         = require('gulp');
+const sass         = require('gulp-sass');
+const sourcemaps   = require('gulp-sourcemaps');
+const postcss      = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano      = require('gulp-cssnano');
+const rename       = require('gulp-rename');
+const runSequence  = require('run-sequence');
+const concat       = require('gulp-concat');
+const clean        = require('gulp-clean');
+const watch        = require('gulp-watch');
+const pug          = require('gulp-pug');
+const sassLint     = require('gulp-sass-lint');
+const gutil        = require('gulp-util');
+
+const _browsers = [ '> 5%', 'last 3 versions' ];
+
+const _processors = [
+  autoprefixer({ browsers: _browsers })
+];
+
+const _jadeLocals = {
+  pageTitle: 'flex _ e _ ble'
+};
+
+const _paths = {
+  scss       : './src/scss/**/*.scss',
+  build      : './dist',
+  build_css  : './dist/css',
+  build_scss : [
+    './src/scss/modules/_vars.scss',
+    './src/scss/modules/_functions.scss',
+    './src/scss/modules/_mixins.scss',
+    './src/scss/modules/_build_container.scss',
+    './src/scss/modules/_build_rows.scss',
+    './src/scss/modules/_build_grids.scss',
+    './src/scss/modules/_grid_states.scss',
+    './src/scss/modules/_vars.scss'
+  ],
+  dist_file  : '_flex_e_ble.scss',
+  jade       : './src/tmpl/*.jade',
+  build_docs : './dist/docs',
+  built_css  : ['./dist/css/*.css', '!./dist/css/*.min.css']
+};
+
+gulp.task( 'sass', () =>
+  gulp.src( _paths.scss )
+  .pipe( sass({ includePaths: 'node_modules', outputStyle: 'expanded' })
+  .on('error', sass.logError))
+  .pipe( postcss( _processors ) )
+  .pipe( sourcemaps.write() )
+  .pipe( gulp.dest( _paths.build_css ))
+);
+
+gulp.task( 'minify', () =>
+  gulp.src( _paths.built_css )
+  .pipe( cssnano({ safe: true }) )
+  .pipe( rename({ suffix: '.min' }) )
+  .pipe( gulp.dest( _paths.build_docs ))
+);
+
+gulp.task( 'merge', () =>
+  gulp.src( _paths.build_scss )
+    .pipe( concat( _paths.dist_file ) )
+    .pipe( gulp.dest( _paths.build ) )
+);
+
+gulp.task( 'jade', () =>
+  gulp.src( _paths.jade )
+  .pipe( pug({ locals: _jadeLocals, pretty: true }) )
+  .pipe( gulp.dest( _paths.build_docs ) )
+);
+
+
+gulp.task( 'sassLint', ['sass'], () =>
+  gulp.src( _paths.scss )
+  .pipe( sassLint() )
+  .pipe( sassLint.format() )
+  .pipe( sassLint.failOnError() )
+);
+
+gulp.task( 'clean', () =>
+  gulp.src( _paths.build, { read: false } )
+  .pipe( clean() )
+);
+
+
+gulp.task( 'watch', (cb) => {
+  watch( _paths.scss, () => gulp.start( 'sassLint' ) );
+  watch( _paths.jade, () => gulp.start('jade') );
+  cb();
+});
+
+
+gulp.task( 'build', ['jade', 'sass', 'merge']);
+
+gulp.task( 'watch', () => runSequence( 'build', 'watch') );
+
+gulp.task( 'lint', ['sassLint']);
+
+gulp.task( 'default', () =>
+  runSequence(['clean'], ['build'], ['merge'], ['minify'])
+);
