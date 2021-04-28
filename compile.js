b10ae9433a91835e60f3968cf38c0ffdf483dcfa
Jack McNicol <jackmcpickle@gmail.com>
2021-04-28 13:25:30 +0930

    update to use github pages

--- compile.js
+++ compile.js
@@ -6,6 +6,6 @@ const { version } = require('./package.json');
 const renderer = pug.compileFile('./src/tmpl/index.pug', {});
 const output = renderer({ version });
 
-fs.writeFile('./dist/docs/index.html', output, 'utf8', (err) => {
+fs.writeFile('./docs/index.html', output, 'utf8', (err) => {
     console.log('Rendered')
 })
